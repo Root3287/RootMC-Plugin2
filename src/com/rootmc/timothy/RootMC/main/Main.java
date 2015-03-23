3d9914d8d1ace6c10f7eb21df678b77926d4592b
Timothy Gibbons <tjgibbons@rootmc.com>
2015-03-22 21:32:14 -0500

    Started
    Did some work here

--- src/com/rootmc/timothy/RootMC/main/Main.java
+++ src/com/rootmc/timothy/RootMC/main/Main.java
@@ -1,5 +1,7 @@
 package com.rootmc.timothy.RootMC.main;
 
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin{
@@ -19,6 +21,7 @@ public class Main extends JavaPlugin{
 	@Override
 	public void onEnable() {
 		sm.setup(this);
+		PluginManager pm = Bukkit.getServer().getPluginManager();
 	}
 	
 	//Loads When Plugin Disable
